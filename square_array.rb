0592b11e979ead018220558182d50fc7f4150b45
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-15 21:39:46 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,6 @@
 def square_array(numbers)
-  array2 = numbers.each do |num**2|
-    return array2
-  end
+  array2 =[]
+  array2.map(numbers.each do |num**2|)
+  return array2
 end
 
-def square_array(array)
-  arr = []
-  array.each { |i| arr << i ** 2 }
-  arr
-end
